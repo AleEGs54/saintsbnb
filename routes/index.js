3ab4c2609dc698023e45c3bf55948b273d652cce
trystandj <trystandj@gmail.com>
2025-08-02 08:45:19 -0400

    Welcome home message

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,11 @@ router.use('/api-docs', require('./swagger'));
 router.use('/posts', require('./post'));
 router.use('/reservations', require('./reservation'));
 
+router.get("/", (req, res) => {
+  // #swagger.tags = ['Home']
+  // #swagger.description = 'Home page endpoint'
+  res.send("Welcome to the home page!");
+});
+
+
 module.exports = router;
