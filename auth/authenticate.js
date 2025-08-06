f4e7f22475f38913733fa97b24eda58a3f7a77f8
Gabriel <gabriel.dubiela@hotmail.com>
2025-08-05 21:41:10 -0300

    trying to fix authentication in render

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,13 +1,10 @@
 // auth/authenticate.js
 const isAuthenticated = (req, res, next) => {
     // Use req.isAuthenticated() from Passport.js
-    if (req.isAuthenticated()) {
-        return next();
-    }
-
-    return res
-        .status(401)
-        .json({ message: 'You do not have access (Authentication Required).' });
+  if (req.session.user === undefined) {
+    return res.status(401).json({ message: "You do not have access" });
+  }
+  return next();
 };
 
 module.exports = { isAuthenticated };
