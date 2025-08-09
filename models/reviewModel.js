bd3f24623b3bb7b673b97509829d1abb45892036
Natnael31 <81311479+Natnael31@users.noreply.github.com>
2025-08-09 19:40:43 +0300

    removed review_id because it is auto-generated by mongo_db

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const reviewSchema = new Schema({
-    review_id: { type: Schema.Types.ObjectId, required: true },
     user_id: { type: Schema.Types.ObjectId, required: true },
     post_id: { type: Schema.Types.ObjectId, required: true },
     review_date: { type: Date, required: true },
